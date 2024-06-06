36841624e3db1afb41b6e11e2e003e28dbdd01ab
erimendes <eri.mendes32@gmail.com>
2024-06-06 15:35:44 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(Strig[] args) {
+    System.out.println("Hello world");
+    // byte -127 a 126
+    // short -32768 a 32767
+    // int -2b a 2b
+    // long -9t a 9t
+}
